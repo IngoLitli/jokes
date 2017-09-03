4efae9a6b7cfd1da6083e07f4fc630eed0cbfcec
IngoLitli <ingvaroli97@hotmail.com>
2017-09-03 13:22:17 +0000

    Update index.php

--- categories/index.php
+++ categories/index.php
@@ -1,34 +1,28 @@
-<?php include_once $_SERVER['DOCUMENT_ROOT'] . '/nemendur/ingvaroli/jokes/authors/worker.php'?>
+<?php include_once $_SERVER['DOCUMENT_ROOT'] . '/nemendur/ingvaroli/jokes/categories/worker.php'?>
 
 <?php include_once $_SERVER['DOCUMENT_ROOT'] . '/nemendur/ingvaroli/jokes/workers/htmlSpecialChars.php'?>
 
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Manage Authors</title>
-    <link rel="stylesheet" type="text/css" href="../css/master.css">
-    <link rel="stylesheet" type="text/css" href="../css/author.css">
-</head>
-<body>
-<h1>Manage Authors</h1>
-<p class="addNewButton"><a href="worker.php?add">Add new author</a></p>
-<table>
-    <tr>
-        <td class="title">Name</td>
-        <td class="title">Email</td>
-    </tr>
-    <?php foreach ($authors as $author): ?>
-        <form action="" method="post">
-            <tr>
-                <td><?php echo html($author['name']); ?></td>
-                <td><?php echo html($author['email']); ?></td>
-                <td><input type="hidden" name="id" value="<?php echo $author['id']; ?>" id="name"></td>
-                <td><input type="submit" name="action" value="Edit" id="editButton" class="button"></td>
-                <td><input type="submit" name="action" value="Delete" id="deleteButton" class="button"></td>
-            </tr>
-        </form>
-    <?php endforeach; ?>
-</table>
-<p class="homeButton"><a href="..">Return to home</a></p>
-</body>
+    <head>
+        <title>Manage Categories</title>
+        <link rel="stylesheet" href="../css/master.css" type="text/css">
+    </head>
+    <body>
+        <h1>Manage Categories</h1>
+        <p class="addNewButton"><a href="worker.php?add">Add new category</a></p>
+        <table>
+            <?php foreach ($categories as $category):?>
+                <form action="" method="post">
+                <tr>
+                    <td><?php echo html($category['name']); ?></td>
+                    <input type="hidden" name="id" value="<?php echo $category['id']; ?>" id="name">
+                    <td><input type="submit" name="action" value="Edit" id="editButton" class="button"></td>
+                    <td><input type="submit" name="action" value="Delete" id="deleteButton" class="button"></td>
+                </tr>
+                </form>
+            <?php endforeach; ?>
+        </table>
+        <p class="homeButton"><a href="..">Return to home</a></p>
+    </body>
 </html>
