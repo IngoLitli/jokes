266a5c2362bd49f1bd4ebaa59dd311ba4c2905a8
IngoLitli <ingvaroli97@hotmail.com>
2017-09-03 12:52:02 +0000

    Update index.php
    Tók út einn link

--- index.php
+++ index.php
@@ -6,6 +6,5 @@
         <h1>Main Page</h1>
         <h2><a href="categories">Manage Categories</a></h2>
         <h2><a href="authors">Manage Authors</a></h2>
-        <h2><a href="authors_v2">Manage Authors</a></h2>
     </body>
 </html>
